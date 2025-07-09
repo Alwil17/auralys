7fb961826ebe0cebd8ff6ce4b55546d737131930
alwil17 <willialfred24@gmail.com>
2025-07-09 16:36:37 +0000

    feat: create layout component for assessments with navigation stack

---
+++ app/assessments/_layout.tsx
@@ -0,0 +1,28 @@
+import { Stack } from "expo-router";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { AssessmentProvider } from "../../contexts/AssessmentContext";
+
+export default function Layout() {
+    return (
+        <AssessmentProvider>
+            <SafeAreaView style={styles.container}>
+                <Stack
+                        screenOptions={{
+                            headerShown: false,
+                        }}
+                    >
+                        <Stack.Screen name="index" />
+                        <Stack.Screen name="step1" />
+                        <Stack.Screen name="step2" />
+                    </Stack>
+            </SafeAreaView>
+        </AssessmentProvider>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF9F6",
+    },
+});
