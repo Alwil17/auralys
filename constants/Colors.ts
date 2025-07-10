b0cca15363f5c813def321c809314077ca755eb0
alwil17 <willialfred24@gmail.com>
2025-07-10 15:06:31 +0000

    feat: add secondaryText color to Colors for improved styling options

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,6 +2,7 @@ const primaryColor = "#e1a2f7";
 const primaryBackgroundColor = "#fffaf4";
 const primaryTextColor = "#232D5C";
 const secondaryColor = "#E8DDD9";
+const secondaryTextColor = "#4F3422";
 
 export const Colors = {
   light: {
@@ -12,5 +13,6 @@ export const Colors = {
     blue: "#104cfe",
     borderColor: "#C9C7C5",
     secondaryBackground: secondaryColor,
+    secondaryText: secondaryTextColor,
   },
 };
