68071d476475927d6179e9f31ca537137c4f4eef
alwil17 <willialfred24@gmail.com>
2025-07-14 09:20:44 +0000

    feat: add step3 screen to navigation in Layout component

--- app/assessments/_layout.tsx
+++ app/assessments/_layout.tsx
@@ -14,6 +14,7 @@ export default function Layout() {
                         <Stack.Screen name="index" />
                         <Stack.Screen name="step1" />
                         <Stack.Screen name="step2" />
+                        <Stack.Screen name="step3" />
                         <Stack.Screen name="step4" />
                         <Stack.Screen name="step5" />
                         <Stack.Screen name="step7" />
