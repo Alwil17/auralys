50a11de8f2841d5344f9cd0416027a68d3fb8cff
alwil17 <willialfred24@gmail.com>
2025-07-25 19:31:00 +0000

    feat: Add routing enums and extension for application navigation

---
+++ lib/core/router/routes.dart
@@ -0,0 +1,34 @@
+enum APP_PAGES {
+  splash,
+  login,
+  register,
+  home,
+  welcome,
+}
+
+abstract class Routes {
+  static const String splash = "/";
+  static const String login = "/login";
+  static const String register = "/register";
+  static const String home = "/home";
+  static const String welcome = "/welcome";
+}
+
+extension AppPageExtension on APP_PAGES {
+  String get toName => name.toUpperCase();
+
+  String get toPath {
+    switch (this) {
+      case APP_PAGES.splash:
+        return Routes.splash;
+      case APP_PAGES.login:
+        return Routes.login;
+      case APP_PAGES.register:
+        return Routes.register;
+      case APP_PAGES.home:
+        return Routes.home;
+      case APP_PAGES.welcome:
+        return Routes.welcome;
+    }
+  }
+}
