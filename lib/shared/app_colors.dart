3bcdc1832f216a27b6661f44b3a535a9a81b25a4
alwil17 <willialfred24@gmail.com>
2025-07-26 17:17:46 +0000

    feat: Add AppColors class for light and dark theme color management

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Theme Colors
+  static const light = _LightColors();
+
+  // Dark Theme Colors
+  static const dark = _DarkColors();
+}
+
+class _DarkColors {
+  const _DarkColors();
+
+  Color get background => const Color(0xFF251404);
+  Color get onBackground => const Color(0xFFFFFFFF);
+  Color get onPrimaryContainer => const Color(0xFFC9C7C5);
+  Color get primary => const Color(0xFF926247);
+  Color get primaryContainer => const Color(0xFF372315);
+  Color get secondary => const Color(0xFF9db068);
+}
+
+class _LightColors {
+  const _LightColors();
+
+  Color get background => const Color(0xFFFAF4FF);
+  Color get onBackground => const Color(0xFF4F3422);
+  Color get onPrimaryContainer => const Color(0xFF736B66);
+  Color get primary => const Color(0xFF4F3422);
+  Color get primaryContainer => const Color(0xFFFFFFFF);
+  Color get secondary => const Color(0xFF9db068);
+}
