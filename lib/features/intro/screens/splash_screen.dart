2cfc24b8f595e1d9853a219fafbb04c84feed6b1
alwil17 <willialfred24@gmail.com>
2025-07-28 14:38:56 +0000

    feat: Refactor SplashScreen to use StatefulWidget and improve navigation logic

--- lib/features/intro/screens/splash_screen.dart
+++ lib/features/intro/screens/splash_screen.dart
@@ -3,26 +3,38 @@ import 'package:go_router/go_router.dart';
 
 import '../../../core/router/routes.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3), () {
-      // Navigate to the loader screen after the splash screen
-      context.pushNamed(APP_PAGES.loader.toName);
-    });
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
         child: Image.asset(
           Theme.of(context).brightness == Brightness.light
-              ? 'assets/icons/splash-icon-light.png'
-              : 'assets/icons/splash-icon-dark.png',
+              ? 'assets/icons/splash-icon-dark.png'
+              : 'assets/icons/splash-icon-light.png',
           width: 200,
           height: 200,
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        context.pushReplacementNamed(APP_PAGES.loader.toName);
+      }
+    });
   }
 }
