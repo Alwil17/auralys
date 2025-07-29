ec0ab15700df07121d70bb503dbcdf28f5a9da2f
alwil17 <willialfred24@gmail.com>
2025-07-29 12:06:39 +0000

    feat: Add AppAssets class for centralized asset management

---
+++ lib/shared/app_assets.dart
@@ -0,0 +1,12 @@
+class AppAssets {
+  // Icons
+  static const String logo = 'assets/icons/logo.png';
+  static const String iosLight = 'assets/icons/ios-light.png';
+  
+  // Images
+  static const String luma = 'assets/images/luma.png';
+  static const String forgotPassword = 'assets/images/forgot_password.png';
+  
+  // Private constructor to prevent instantiation
+  AppAssets._();
+}
