7c751c4ea7d5dabf79c49316c823908e58019f74
alwil17 <willialfred24@gmail.com>
2025-08-04 15:59:02 +0000

    refactor: Clean up StepContent widget structure for improved readability

--- lib/features/assessments/widgets/step_content.dart
+++ lib/features/assessments/widgets/step_content.dart
@@ -11,12 +11,14 @@ class StepContent extends StatefulWidget {
   final int stepIndex;
   final VoidCallback? onNextStep;
   final VoidCallback? onPreviousStep;
+  final ValueChanged<String>? onNameChanged;
 
   const StepContent({
     super.key, 
     required this.stepIndex, 
     this.onNextStep,
     this.onPreviousStep,
+    this.onNameChanged,
   });
 
   @override
@@ -193,7 +195,7 @@ class _StepContentState extends State<StepContent> {
       case 6:
         return _MoodAnalysisPermissionStep();
       case 7:
-        return const NameStep();
+        return NameStep(onNameChanged: widget.onNameChanged);
       default:
         return _buildPlaceholderStep(step);
     }
