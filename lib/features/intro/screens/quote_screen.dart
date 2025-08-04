cc0c098fe9e47b52fec2e6aade739e8234e3d29b
alwil17 <willialfred24@gmail.com>
2025-08-04 10:50:25 +0000

    fix: Update color opacity method in QuoteScreen widget to use withValues

--- lib/features/intro/screens/quote_screen.dart
+++ lib/features/intro/screens/quote_screen.dart
@@ -50,7 +50,7 @@ class _QuoteScreenState extends State<QuoteScreen>
                         color: Colors.white,
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.black.withOpacity(0.1),
+                            color: Colors.black.withValues(alpha: 0.1),
                             blurRadius: 8,
                             offset: const Offset(0, 2),
                           ),
